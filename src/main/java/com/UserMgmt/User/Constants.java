fd2a9c5e447d3cec61b577e86565e829c7b8fc82
unknown <priyanka@nishkaiv.com>
2025-03-21 13:14:11 +0530

    added constant.java

---
+++ src/main/java/com/UserMgmt/User/Constants.java
@@ -0,0 +1,7 @@
+package com.UserMgmt.User;
+
+public class Constants {
+	
+	public static String DATA_TYPE ="JavaTechie";
+
+}
--- src/main/java/com/UserMgmt/User/HelloController.java
+++ src/main/java/com/UserMgmt/User/HelloController.java
@@ -25,6 +25,7 @@ public class HelloController {
 	public void msg()
 	{
 		System.out.println("Welcome");
+		System.out.println("DataType is : " + Constants.DATA_TYPE);
 		
 	}
 
